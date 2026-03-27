c028ead80884fe67d497349921637fbbb0948c6f
DikryRamdani <rmdnidkry@gmail.com>
2026-03-28 00:41:25 +0700

    fix kesekian kalinya

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "/api";
+const BASE_URL = "https://gardenofhabits.my.id/api";
 
 const apiFetch = async (endpoint, options = {}) => {
   const token = localStorage.getItem("access_token");
